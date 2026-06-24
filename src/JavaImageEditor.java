e9ec941b3be18c8e4be7bfbcfc927b2bea7b6571
Miguel Dinis <mfadinis2007@gmail.com>
2026-06-24 18:03:50 +0100

    Small Alterations

--- src/JavaImageEditor.java
+++ src/JavaImageEditor.java
@@ -1,69 +1,80 @@
-// Projeto de IP - Miguel Ferreira Alves Dinis - nº138808
 import pt.iscte.greyditor.Greyditor;
 import pt.iscte.greyditor.Editor;
 import pt.iscte.greyditor.Selection;
+
 void main() {
-    Greyditor configuration = new Greyditor("ProjetoIP");
+    Greyditor configuration = new Greyditor("JavaImageEditor");
     configuration.addFilter("Invert", this::invert);
-    configuration.addFilter("Darken/Brighten", this::DarkenBrighten, -255, 255);
-    configuration.addFilter("Contrast", this::Contrast, 0, 255);
-    configuration.addFilter("Grain", this::Grain,0,255);
+    configuration.addFilter("Darken/Brighten", this::darkenBrighten, -255, 255);
+    configuration.addFilter("Contrast", this::contrast, 0, 255);
+    configuration.addFilter("Grain", this::grain,0,255);
     configuration.addEffect("Grid", this::grid);
     configuration.addEffect("Lines", this::lines, 0, 50);
-    configuration.addEffect("Vignette (Darken/Brighten)", this::Vignette, -255, 255);
-    configuration.addEffect("Horizontal Mirror", this::HorizontalMirror);
-    configuration.addEffect("Vertical Mirror", this::VerticalMirror);
-    configuration.addEffect("Margin", this::Margin);
+    configuration.addEffect("Vignette (Darken/Brighten)", this::vignette, -255, 255);
+    configuration.addEffect("Horizontal Mirror", this::horizontalMirror);
+    configuration.addEffect("Vertical Mirror", this::verticalMirror);
+    configuration.addEffect("Margin", this::margin);
     configuration.addOperation("Square", this::square);
     configuration.addOperation("Darken Area", this::darkenArea);
-    configuration.addOperation("Crop", this::Crop);
-    configuration.addOperation("Expand", this::Expand);
-    configuration.addOperation("Posterize", this::Posterize);
-    configuration.addOperation("Rotate", this::Rotate);
-    configuration.addOperation("Blur", this::GetBlurRadius);
-    configuration.addOperation("Ancient Filter", this::Ancient);
-    configuration.addOperation("Retro Filter", this::Retro);
-    configuration.addOperation("Copy", this::Copy);
-    configuration.addOperation("Cut", this::Cut);
-    configuration.addOperation("Paste", this::Paste);
-    configuration.addOperation("Undo", this::Undo);
+    configuration.addOperation("Crop", this::crop);
+    configuration.addOperation("Expand", this::expand);
+    configuration.addOperation("Posterize", this::posterize);
+    configuration.addOperation("Rotate", this::rotate);
+    configuration.addOperation("Blur", this::getBlurRadius);
+    configuration.addOperation("Ancient Filter", this::ancientFilter);
+    configuration.addOperation("Retro Filter", this::retroFilter);
+    configuration.addOperation("Copy", this::copy);
+    configuration.addOperation("Cut", this::cut);
+    configuration.addOperation("Paste", this::paste);
+    configuration.addOperation("Undo", this::undo);
     configuration.addLoadOperation("Load");
     configuration.addSaveOperation("Save");
     configuration.open("monalisa.jpg");
 }
-int[][] beforeImage; // Variável para ser usada no Undo
-int[][] copiedImage; // Variável para ser usada no Copy
-boolean isCopied = false; // Variável para determinar se é Copy
-boolean isCut = false; // Variável para determinar se é Cut
-int cutTimes = 0; // Variável para determinar as vezes que o utilizador fez Cut
-boolean action = false; // Variável que determina se algo já foi feito (para o Undo)
-void save(int[][] image){ // Função de guardar
-    beforeImage = new int[image.length][image[0].length]; // Variável que guarda
+
+int[][] beforeImage; // variable to be used in undo
+
+int[][] copiedImage; // variable to be used in copy
+
+boolean isCopied = false; // variable to determine if it's copied
+
+boolean isCut = false; // variable to determine if it's cut
+
+int cutTimes = 0; // variable to determine how many times the user has used cut
+
+boolean action = false; // variable to determine if something has happened (for the cut function)
+
+void save(int[][] image){ // save function to save an image before an alteration
+    beforeImage = new int[image.length][image[0].length];
     for(int y = 0; y<image.length; y++){
         for(int x = 0; x<image[y].length; x++){
-            beforeImage[y][x]=image[y][x]; // Iteração para igual cada pixel à imagem dada
+            beforeImage[y][x]=image[y][x];
         }
     }
-    action=true; // Determinar que algo já foi feito
+    action=true;
 }
+
 int invert(int tone) { // Função que inverte o tom de cada pixel
     return 255 - tone;
-}
-int DarkenBrighten(int tone, int intensity) { // Função que escurece ou clareia cada pixel
+} // invert function that replaces the tone of each pixel with (255-tone)
+
+int darkenBrighten(int tone, int intensity) { // function that darkens/brightens every pixel
     if(intensity>=0) {
         return Math.min(255, tone+intensity);
     }else{
         return Math.max(0, tone+intensity);
     }
 }
-int Contrast(int tone, int intensity){ // Função que torna os pixeis claros mais claros e os pixeis escuros mais escuros
+
+int contrast(int tone, int intensity){ // function that highlights the darkness and lightness of the image
     if(tone>255/2) {
         return Math.min(255, tone+intensity);
     }else{
         return Math.max(0, tone-intensity);
     }
 }
-void grid(int[][] image) { // Função que desenha uma Grid sobre a imagem
+
+void grid(int[][] image) { // function that draws a grid over the image
     int hspace = (image[0].length + 2) / 3;
     int vspace = (image.length + 2) / 3;
     for (int y = vspace; y < image.length; y += vspace)
@@ -73,14 +84,16 @@ void grid(int[][] image) { // Função que desenha uma Grid sobre a imagem
         for (int y = 0; y < image.length; y++)
             image[y][x] = 200;
 }
-void lines(int[][] image, int spacing) { // Função que desenha linhas sobre a imagem
+
+void lines(int[][] image, int spacing) { // function that draws lines over the image
     if (spacing == 0)
         return;
     for (int y = 0; y < image.length; y += spacing)
         for (int x = 0; x < image[y].length; x++)
             image[y][x] = 0;
 }
-int[][] square(int[][] image) { // Função que transforma a imagem num quadrado
+
+int[][] square(int[][] image) { // function that sets the resolution to a square form
     save(image);
     int side = Math.min(image.length, image[0].length);
     int[][] square = new int[side][side];
@@ -90,7 +103,8 @@ int[][] square(int[][] image) { // Função que transforma a imagem num quadrado
 
     return square;
 }
-int[][] darkenArea(int[][] image, Editor editor) { // Função que escurece uma parte da imagem selecionada
+
+int[][] darkenArea(int[][] image, Editor editor) { // function that darkens the selected part of the image
     Selection selection = editor.getSelection();
     if (selection == null) {
         editor.message("Please select an area of the image.");
@@ -107,7 +121,8 @@ int[][] darkenArea(int[][] image, Editor editor) { // Função que escurece uma 
     }
     return null;
 }
-void HorizontalMirror(int[][] image){ // Função que espelha a imagem horizontalmente
+
+void horizontalMirror(int[][] image){ // function that mirrors the image horizontal
     for(int y = 0; y<image.length/2; y++){
         int invert = image.length-1-y;
         int[] temp = image[y];
@@ -115,7 +130,8 @@ void HorizontalMirror(int[][] image){ // Função que espelha a imagem horizonta
         image[invert] = temp;
     }
 }
-void VerticalMirror(int[][] image) { // Função que espelha a imagem verticalmente
+
+void verticalMirror(int[][] image) { // function that mirrors the image vertical
     for (int y = 0; y < image.length; y++) {
         for (int x = 0; x < image[y].length / 2; x++) {
             int invert = image[y].length - 1 - x;
@@ -125,13 +141,15 @@ void VerticalMirror(int[][] image) { // Função que espelha a imagem verticalme
         }
     }
 }
-int Grain(int tone, int intensity) { // Função que clareia (sensivelmente) metado dos pixeis e escurece a outra metade
+
+int grain(int tone, int intensity) { // function that brightens (around) half and darkens the other half of the image
     if(Math.random()>.5) {
         return Math.max(0,tone-intensity);
     }
     return Math.min(255, tone+intensity);
 }
-int[][] Margin(int[][] image){ // Função que cria uma margem sobre a imagem
+
+int[][] margin(int[][] image){ // function that creates a margin over the image
     for(int y = 0; y<image.length; y++){
         for(int x = 0; x<image[y].length; x++){
             if(x<28||y<28) {
@@ -143,7 +161,8 @@ int[][] Margin(int[][] image){ // Função que cria uma margem sobre a imagem
     }
     return image;
 }
-int[][] Vignette(int[][] image, int intensity) { // Função que cria uma vinheta (clara ou escura) sobre a imagem
+
+int[][] vignette(int[][] image, int intensity) { // function that creates a vignette over the image
     int width = image[0].length;
     int height = image.length;
     int centerX = width / 2;
@@ -160,7 +179,8 @@ int[][] Vignette(int[][] image, int intensity) { // Função que cria uma vinhet
     }
     return image;
 }
-int[][] Crop(int[][] image, Editor editor){ // Função que aproxima para uma parte selecionada da imagem
+
+int[][] crop(int[][] image, Editor editor){ // function that crops into the selected part of the image
     Selection selection = editor.getSelection();
     int[][] crop = new int[0][0];
     if (selection == null) {
@@ -181,7 +201,8 @@ int[][] Crop(int[][] image, Editor editor){ // Função que aproxima para uma pa
     }
     return null;
 }
-int[][] Expand (int[][] image, Editor editor) { // Função que expande a imagem
+
+int[][] expand (int[][] image, Editor editor) { // function that alters the resolution of the image to a bigger one
     int altura = 0;
     int largura = 0;
     while((altura<=image.length || largura <=image[0].length) || (altura>1000 || largura>1000)){
@@ -194,7 +215,7 @@ int[][] Expand (int[][] image, Editor editor) { // Função que expande a imagem
             editor.message("The expansion must be shorter than 1000x1000.");
         }
     }
-    int[][] expandedImage = new int[altura][largura]; // criação da imagem expandida
+    int[][] expandedImage = new int[altura][largura];
     save(image);
     for(int y = 0; y<image.length; y++){
         for(int x = 0; x<image[y].length; x++){
@@ -203,7 +224,8 @@ int[][] Expand (int[][] image, Editor editor) { // Função que expande a imagem
     }
     return expandedImage;
 }
-int[][] Posterize(int[][] image, Editor editor){ // Função que determina um certo número de cores para a imagem
+
+int[][] posterize(int[][] image, Editor editor){ // function that determines the number of tones in the image
     int colors = 0;
     while(colors<=1||colors>20) {
         colors = editor.getInteger("Colors?");
@@ -227,7 +249,8 @@ int[][] Posterize(int[][] image, Editor editor){ // Função que determina um ce
     }
     return posterizedImage;
 }
-int[][] Rotate (int[][] image){ // Função que roda a imagem a 90º graus para a direita
+
+int[][] rotate (int[][] image){ // function that rotates the image 90º
     int[][] rotatedImage = new int[image[0].length][image.length];
     save(image);
     for(int y = 0; y<image.length; y++){
@@ -241,7 +264,8 @@ int[][] Rotate (int[][] image){ // Função que roda a imagem a 90º graus para 
     }
     return rotatedImage;
 }
-int[][] Blur(int[][] image, int r){ // Função que provoca um blur à imagem com um raio
+
+int[][] blur(int[][] image, int r){ // function that turns the image into a blurred image with a radius
     int[][] blurredImage = new int[image.length][image[0].length];
     save(image);
     for(int y = 0; y<image.length; y++){
@@ -261,7 +285,8 @@ int[][] Blur(int[][] image, int r){ // Função que provoca um blur à imagem co
     }
     return blurredImage;
 }
-int[][] GetBlurRadius(int[][] image, Editor editor){ // Função para adquirir o raio para o Blur
+
+int[][] getBlurRadius(int[][] image, Editor editor){ // function to get the radius to blur
     int r = 0;
     while(r<1||r>10) {
         r = editor.getInteger("Radius?");
@@ -272,27 +297,30 @@ int[][] GetBlurRadius(int[][] image, Editor editor){ // Função para adquirir o
             editor.message("The radius must be smaller than 10.");
         }
     }
-    return Blur(image, r);
+    return blur(image, r);
 }
-int[][] Ancient(int[][] image){ // Função para o filtro Ancient que utiliza o Grain, o Margin e a Vignette
+
+int[][] ancientFilter(int[][] image){ // function for the ancient filter that uses grain, margin and vignette
     save(image);
     for(int y = 0; y<image.length; y++){
         for(int x = 0; x<image[y].length; x++){
-            image[y][x]=Grain(image[y][x],20);
+            image[y][x]=grain(image[y][x],20);
         }
     }
-    return Margin(Vignette(image,150));
+    return margin(vignette(image,150));
 }
-int[][] Retro(int[][] image){ // Função para o filtro Retro que utiliza o Contrast, o Blur e a Vignette
+
+int[][] retroFilter(int[][] image){ // function for the retro filter that uses contrast, blur and vignette
     save(image);
     for(int y = 0; y<image.length; y++){
         for(int x = 0; x<image[y].length; x++){
-            image[y][x]=Contrast(image[y][x],70);
+            image[y][x]=contrast(image[y][x],70);
         }
     }
-    return Blur(Vignette(image, 150),3);
+    return blur(vignette(image, 150),3);
 }
-int[][] Copy(int[][] image, Editor editor){ // Função que copia uma parte da imagem selecionada
+
+int[][] copy(int[][] image, Editor editor){ // function that copies the selected part of the image
     Selection selection = editor.getSelection();
     if (selection == null) {
         editor.message("Please select an area of the image.");
@@ -313,7 +341,8 @@ int[][] Copy(int[][] image, Editor editor){ // Função que copia uma parte da i
     }
     return null;
 }
-int[][] Cut(int[][] image, Editor editor){ // Função que apaga e copia uma parte da imagem selecionada
+
+int[][] cut(int[][] image, Editor editor){ // function that erases and copies the selected part of the image
     Selection selection = editor.getSelection();
     if (selection == null) {
         editor.message("Please select an area of the image.");
@@ -337,7 +366,8 @@ int[][] Cut(int[][] image, Editor editor){ // Função que apaga e copia uma par
     }
     return null;
 }
-int[][] Paste(int[][] image, Editor editor) { // Função que cola uma parte da imagem copiada (com copy ou cut)
+
+int[][] paste(int[][] image, Editor editor) { // function that pastes a copied part of the image (through copy/cut)
     Selection selection = editor.getSelection();
     if (selection == null) {
         editor.message("Please select an area of the image.");
@@ -389,7 +419,8 @@ int[][] Paste(int[][] image, Editor editor) { // Função que cola uma parte da 
     }
     return image;
 }
-int[][] Undo(int[][] image, Editor editor){ // Função que volta atrás (apenas uma vez)
+
+int[][] undo(int[][] image, Editor editor){ // function that undoes only the previous alteration
     if(beforeImage!=null){
         if(!action) {
             editor.message("Software in progress, you can only undo once, please wait patiently for more updates :)");
